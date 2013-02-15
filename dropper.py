e8b85d0a5d61d0d800fa981685ff138e0cfc00b0
Scott Behrens <sbehrens@gmail.com>
2013-02-15 15:52:55 -0600

    more stuff

--- dropper.py
+++ dropper.py
@@ -7,15 +7,16 @@ def createSSHClient(server, port, user, password):
     client = paramiko.SSHClient()
     client.load_system_host_keys()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(server, port, user, password)
+    client.connect(server, port, user, password, timeout=3)
     return client
 
 try:
 
-    ssh = createSSHClient("127.0.0.1", 22, "sbehrens", "candiria7xx18")
+    ssh = createSSHClient("10.40.122.172", 22, "sbehrens", "candiria718")
 
 except Exception as inst:
 
+    # print hostname with error
     print type(inst)
     print inst.args
     print inst
@@ -23,6 +24,7 @@ except Exception as inst:
      
 scp = SCPClient(ssh.get_transport())
 transport = ssh.get_transport()
+try:
 scp.put('/tmp/foobar', '/tmp/test')
 channel = transport.open_session()
-channel.exec_command("ls /tmp > /tmp/ididthis")
+channel.exec_command("ls /opt > /tmp/ididthis")
